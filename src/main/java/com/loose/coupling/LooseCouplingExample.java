5501b64979ba0c777f4aefc8476835a8e9024173
Ashish Kumar Pandey <akpandey369936@gmail.com>
2026-01-30 23:40:14 +0530

    fetching from database

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -5,5 +5,12 @@ public class LooseCouplingExample {
         UserDataProvider databaseProvider=new UserDatabaseProvider();
         UserManager userManagerWithDB=new UserManager(databaseProvider);
         System.out.println(userManagerWithDB.getUserInfo());
+        UserDataProvider webserviceProvider=new WebServiceDataProvider();
+        UserManager userManagerWithWebservice=new UserManager(webserviceProvider);
+        System.out.println(userManagerWithWebservice.getUserInfo());
+
+        UserDataProvider newDatabaseProvider=new NewDatabaseProvider();
+        UserManager newDatabaseManagerWithNewDB=new UserManager(newDatabaseProvider);
+        System.out.println(newDatabaseManagerWithNewDB.getUserInfo());
     }
 }
