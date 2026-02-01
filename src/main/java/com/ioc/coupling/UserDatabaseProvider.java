0955251fe2a91a329cae5a763fd31caab00b0d3b
Ashish Kumar Pandey <akpandey369936@gmail.com>
2026-02-01 09:41:02 +0530

    Implemented loose coupling using Spring IoC container

---
+++ src/main/java/com/ioc/coupling/UserDatabaseProvider.java
@@ -0,0 +1,9 @@
+package com.ioc.coupling;
+
+public class UserDatabaseProvider implements UserDataProvider {
+    @Override
+    public String getUserDetails(){
+        //directly access database here
+        return "User Details from the Database";
+    }
+}
