59e4faf1d334cfd26aa4ca9779aac422532a9284
Ashish Kumar Pandey <akpandey369936@gmail.com>
2026-02-02 08:31:28 +0530

    Implemented annotation-based dependency injection

---
+++ src/main/java/com/example/autowired/annotation/App.java
@@ -0,0 +1,14 @@
+package com.example.autowired.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Employee employee = (Employee) context.getBean("employee");
+
+        Manager manager = (Manager) context.getBean("manager");
+        System.out.println(manager.toString());
+    }
+}
